c4d02d45a8f3f46010e32874541f7c4d7db0be8d
lxf <1030917233@qq.com>
2018-06-26 15:37:13 +0800

    add webHook error

--- RaiseSalary/LearnJenkinsGithub/JenkinsGithub.py
+++ RaiseSalary/LearnJenkinsGithub/JenkinsGithub.py
@@ -5,3 +5,4 @@ Created on 2018年6月26日
 @author: vn0umhp
 '''
 print "2018年6月26日testJenkins自动构建---lxf"
+print "github 项目中的Settings中 Webhooks中报错：Last delivery was not successful. couldn't connect to server."
