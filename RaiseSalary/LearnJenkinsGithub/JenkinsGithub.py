2db71b9e25fe392d10781d994b887daa8e805bad
lxf <1030917233@qq.com>
2018-06-27 10:07:20 +0800

    add Jenkins webhook

--- RaiseSalary/LearnJenkinsGithub/JenkinsGithub.py
+++ RaiseSalary/LearnJenkinsGithub/JenkinsGithub.py
@@ -6,3 +6,4 @@ Created on 2018年6月26日
 '''
 print "2018年6月26日testJenkins自动构建---lxf"
 print "github 项目中的Settings中 Webhooks中报错：Last delivery was not successful. couldn't connect to server."
+print "勾选Jenkins中的webhook"
